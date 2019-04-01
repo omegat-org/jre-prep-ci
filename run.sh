7361ea8315be0c8f5ae0da200dff73f81f27d80d
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-04-01 22:17:03 +0900

    Put downloads in dist dir

--- run.sh
+++ run.sh
@@ -6,6 +6,9 @@ jres=(https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5b
     https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jre-8u201-windows-i586.tar.gz
     https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jre-8u201-windows-x64.tar.gz)
 
+mkdir -p dist
+cd dist
+
 for url in "${jres[@]}"; do
     curl -L -b "oraclelicense=a" -O "$url"
 done
